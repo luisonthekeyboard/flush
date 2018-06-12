54e0e277a75ad90cde7f246becbe7aea7180f70d
Luis <luis@luisonthekeyboard.com>
2018-06-12 20:53:47 +0200

    Rush loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Load config files, if any.
+
+    // Run command loop.
+    rush_loop();
+
+    // Perform any shutdown/cleanup.
+}
+
+fn rush_loop() {
+    let mut input = String::new();
+
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{} bytes read", n);
+                println!("{}", input);
+            }
+            Err(error) => println!("error: {}", error),
+        }
+    }
 }
