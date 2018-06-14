d77e8fbf701d9e83b367c7998e57930e59947ea0
Luis <luis@luisonthekeyboard.com>
2018-06-14 12:10:25 +0200

    Split line into command and arguments

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,45 @@ fn main() {
 
 fn rush_loop() {
     loop {
+        let mut input = String::new();
         print!(">>>> ");
         io::stdout().flush().unwrap();
 
-        let line = rush_read_line();
-        io::stdout().write(line.as_bytes()).unwrap();
-        //        println!("->{}", line);
+        let line = rush_read_line(&mut input);
+        io::stdout().write(line).unwrap();
 
-        // let args = rush_split_line(&line);
-        //  println!("->{}", args);
+        let args = rush_split_line(&line);
+        println!(
+            "-> '{}', '{}'",
+            String::from_utf8_lossy(args.0.as_bytes()),
+            String::from_utf8_lossy(args.1.as_bytes())
+        );
     }
 }
 
-// fn rush_split_line(line: &String) -> [String] {
+fn rush_split_line(line: &[u8]) -> (CString, CString) {
+    for (i, &item) in line.iter().enumerate() {
+        if item == b' ' {
+            unsafe {
+                return (
+                    CString::from_vec_unchecked(line[0..i].to_vec()),
+                    CString::from_vec_unchecked(line[i..line.len()].to_vec()),
+                );
+            }
+        }
+    }
 
-// }
+    unsafe {
+        return (
+            CString::from_vec_unchecked(line.to_vec()),
+            CString::new("").unwrap(),
+        );
+    }
+}
 
-fn rush_read_line() -> CString {
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => return CString::new(input.as_bytes()).unwrap(),
-        Err(_) => return CString::new("error").unwrap(),
+fn rush_read_line(input: &mut String) -> &[u8] {
+    match io::stdin().read_line(input) {
+        Ok(_) => return input.as_bytes(),
+        Err(_) => return "error".as_bytes(),
     }
 }
